18b1c3d85f0808ec4d075db7da002595630e25d7
Maxim <toper.one111@gmail.com>
2023-12-05 14:57:24 +0200

    Display pdf

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,12 +1,53 @@
+'use client';
+
+import { Document, Page, pdfjs } from 'react-pdf';
+import { toast } from './ui/use-toast';
+import { Loader2 } from 'lucide-react';
+import { useState } from 'react';
+
+import { useResizeDetector } from 'react-resize-detector';
+
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 interface PdfRendererProps {
   url: string;
 }
 
 const PdfRenderer = ({ url }: PdfRendererProps) => {
+  const [numPages, setNumPages] = useState<number>();
+  const [pageNumber, setPageNumber] = useState<number>(1);
+
+  const { width, ref } = useResizeDetector();
+
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
         <div className="flex items-center gap-1.5">top bar</div>
+      </div>
+
+      <div className="flex-1 w-full max-h-screen">
+        <div ref={ref}>
+          <Document
+            loading={
+              <div className="flex justify-center">
+                <Loader2 className="my-24 h-6 w-6 animate-spin" />
+              </div>
+            }
+            file={url}
+            onLoadError={() => {
+              toast({
+                title: 'Error loading PDF',
+                description: 'Please try again later',
+                variant: 'destructive',
+              });
+            }}
+          >
+            <Page pageNumber={pageNumber} width={width} />
+          </Document>
+        </div>
       </div>
     </div>
   );
